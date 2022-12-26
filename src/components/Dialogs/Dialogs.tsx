abfd5affff1243b94935e7dae65e8370fdbf7b41
wostrau <a.ostrovtsov@gmail.com>
2022-12-26 18:07:29 +0100

    video lesson #18 - done

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Dialogs.module.css';
+
+export const Dialogs = () => {
+    return (
+        <div>
+            DIALOGS
+        </div>
+    );
+};
