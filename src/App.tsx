eeaf207e5868679563c5cb70843581da085b6f27
wostrau <a.ostrovtsov@gmail.com>
2022-12-27 11:26:49 +0100

    video lesson #19 - done

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,26 @@ import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
 import {Dialogs} from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-            <Profile/>
-            <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route
+                        path="/profile"
+                        component={Profile}
+                    />
+                    <Route
+                        path="/dialogs"
+                        component={Dialogs}
+                    />
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
