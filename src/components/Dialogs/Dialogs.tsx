e245c37791a435834ef246df52374d099dbf7031
wostrau <a.ostrovtsov@gmail.com>
2022-12-27 16:01:59 +0100

    video lesson #21 - done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import styles from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            DIALOGS
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={`${styles.dialog} ${styles.active}`}>DIMYCH</div>
+                <div className={styles.dialog}>ANDREW</div>
+                <div className={styles.dialog}>ANTONI</div>
+                <div className={styles.dialog}>CAPONE</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.dialog}>Hi, how are you?</div>
+                <div className={styles.dialog}>I'm fine, thanks</div>
+                <div className={styles.dialog}>What are your plans?</div>
+                <div className={styles.dialog}>Go for a walk this evening</div>
+            </div>
         </div>
     );
 };
