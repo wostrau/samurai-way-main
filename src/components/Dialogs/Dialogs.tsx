702638a857634083a9f29d7543aae381e300ee48
wostrau <a.ostrovtsov@gmail.com>
2022-12-27 16:14:12 +0100

    video lesson #22 - done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 export const Dialogs = () => {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItems}>
-                <div className={`${styles.dialog} ${styles.active}`}>DIMYCH</div>
-                <div className={styles.dialog}>ANDREW</div>
-                <div className={styles.dialog}>ANTONI</div>
-                <div className={styles.dialog}>CAPONE</div>
+                <div className={`${styles.dialog} ${styles.active}`}>
+                    <NavLink to="/dialogs/1">
+                        DIMYCH
+                    </NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/1">
+                        ANDREW
+                    </NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/1">
+                        ANTONI
+                    </NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/1">
+                        CAPONE
+                    </NavLink>
+                </div>
             </div>
             <div className={styles.messages}>
                 <div className={styles.dialog}>Hi, how are you?</div>
