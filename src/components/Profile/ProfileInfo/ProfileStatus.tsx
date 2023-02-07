f6f9b8a0ba4c1f014e6c41189dfaa6a0dbb5dfe3
wostrau <a.ostrovtsov@gmail.com>
2023-02-07 12:46:58 +0100

    video lesson #74 - DONE

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -25,6 +25,12 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
         this.setState({status: e.currentTarget.value})
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusPropsType>, prevState: Readonly<{}>, snapshot?: any) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status})
+        }
+    }
+
     render() {
         return (
             <>
