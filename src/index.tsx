4ca461c171e69fbe5b81323f9cdca071eeae4fb6
wostrau <a.ostrovtsov@gmail.com>
2023-02-11 22:21:51 +0100

    video lesson #95 - DONE

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
 import {store} from './redux/redux-store';
 import {Provider} from 'react-redux';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
