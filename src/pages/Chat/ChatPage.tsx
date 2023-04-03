c98a1ec3d63bb5f77c9153b8e2ee42601472730f
wostrau <a.ostrovtsov@gmail.com>
2023-04-03 23:27:25 +0200

    Samurai Way v2.0 - lesson #18 - DONE (WebSocket)

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 
 const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandlers.ashx')
@@ -29,11 +29,6 @@ export type ChatMessageType = {
 
 const Chat: React.FC = () => {
 
-    useEffect(() => {
-        ws.addEventListener('message', (e) => {
-            console.log(e)
-        })
-    }, [])
 
     return (
         <div>
@@ -44,27 +39,56 @@ const Chat: React.FC = () => {
 }
 
 const Messages: React.FC = () => {
-    const messages: any[] = []
+
+    const [messages, setMessages] = useState<ChatMessageType[]>([])
+
+    useEffect(() => {
+        ws.addEventListener('message', (e: MessageEvent) => {
+            const newMessages = JSON.parse(e.data)
+            setMessages((prevMessages) => [...prevMessages, ...newMessages])
+        })
+    }, [])
+
 
     return <div style={{height: '400px', overflowY: 'auto'}}>
-        {messages.map((m: any) => <Message/>)}
+        {messages.map((m, index) => <Message key={index} message={m}/>)}
     </div>
 }
 
 const AddMessageForm: React.FC = () => {
+
+    const [message, setMessage] = useState('')
+
+    const sendMessage = () => {
+        if(!message) return
+        ws.send(message)
+        setMessage('')
+    }
+
     return <div>
         <div>
-            <textarea name="sendMessage"></textarea>
+            <textarea
+                name="sendMessage"
+                onChange={e => setMessage(e.currentTarget.value)}
+                value={message}
+            ></textarea>
         </div>
-        <button>Send</button>
+        <button
+            onClick={sendMessage}
+        >Send
+        </button>
     </div>
 }
 
-const Message = () => {
-    const message: ChatMessageType = null
+const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
+
     return (
         <div>
-            <img src={message.photo}/>
+            <img
+                src={message.photo}
+                alt={'authorsAvatar'}
+                style={{width: '30px'}}
+            />
             <b>{message.userName}</b>
             <br/>
             {message.message}
